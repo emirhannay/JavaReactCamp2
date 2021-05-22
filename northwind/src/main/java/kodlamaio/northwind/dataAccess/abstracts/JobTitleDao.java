319db985aaf6503555344f0ec1e3d09828442b30
Emirhan Ay <69421949+emirhannay@users.noreply.github.com>
2021-05-22 18:13:43 +0300

    Delete northwind directory

--- northwind/src/main/java/kodlamaio/northwind/dataAccess/abstracts/JobTitleDao.java
+++
@@ -1,9 +0,0 @@
-package kodlamaio.northwind.dataAccess.abstracts;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import kodlamaio.northwind.entities.concretes.JobTitle;
-
-public interface JobTitleDao extends JpaRepository<JobTitle, Integer> {
-
-}
